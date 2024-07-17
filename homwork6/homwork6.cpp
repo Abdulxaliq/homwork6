21a3a5ce172b68c48d99503fa7a22e0b4d55c770
Abdulxaliq <Agaye_ck17@itstep.edu.az>
2024-07-17 17:52:49 +0400

    Add project files.

---
+++ homwork6/homwork6.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//int eded = 0;
+	//cout << "eded daxil edin; ";
+	//cin >> eded ;
+	//eded % 2 == 0 && cout << "Cut eded" || cout << "Tek eded";
+
+
+	//double num1 = 0;
+	//double num2 = 0;
+	//cout << "1-ci ededi daxil edin: ";
+	//cin >> num1;
+	//cout << "2-ci ededi daxil edin: ";
+	//cin >> num2;
+	//if (num1 > num2) cout << "kicik eded " << num2 << "dir";
+	//else cout << "kicik eded " << num1 << "dir";
+
+
+	//double eded_1 = 0;
+	//cout << "eded daxil edin; ";
+	//cin >> eded_1 ;
+	//if (eded_1 < 0) cout << "Menfi eded";
+	//else if (eded_1 > 0) cout << "Musbet eded";
+	//else cout << "Sifir";
+
+	//float num_1 = 0;
+	//float num_2 = 0;
+	//double num_3 = 0;
+	//cout << "1-ci kesr ededi daxil edin: ";
+	//cin >> num_1;
+	//cout << "2-ci kesr ededi daxil edin: ";
+	//cin >> num_2;
+	//cout << "1) a + b" << endl;
+	//cout << "2) a - b" << endl;
+	//cout << "3) a * b" << endl;
+	//cout << "4) a / b" << endl;
+	//cin >> num_3;
+	//if (num_3 == 1) cout << num_1 + num_2;
+	//else if (num_3 == 2) cout << num_1 - num_2;
+	//else if (num_3 == 3) cout << num_1 * num_2;
+	//else if (num_3 == 4) cout << num_1 / num_2;
+	//else cout << "Sadece ekrandaki ededlerden birini secin!!!";
+
+
+
+	//double eded_2 = 0;
+	//cout << "1-50 arasi eded daxil edin; ";
+	//cin >> eded_2 ;
+	//if (0 < eded_2 < 51) cout << "Daxil etdiyiniz eded araliqdadir";
+	//else cout << "Daxil etdiyiniz eded araliqda deyil!";
+
+
+
+	//int num_4 = 0;
+	//int num_5 = 0;
+	//cout << "1-ci ededi daxil edin: ";
+	//cin >> num_4;
+	//cout << "2-ci ededi daxil edin: ";
+	//cin >> num_5;
+	//if (num_4 % num_5 == 0) cout << "YES";
+	//else cout << "NO";
+
+
+	//int num_6 = 0;
+	//cout << "eded daxil edin: ";
+	//cin >> num_6;
+	//64 < num_6 < 91 && cout << "HERF" || cout << "PUNKT";
+
+
+	//int eded1 = 0;
+	//cout << "1-ci ededi daxil edin";
+	//cin >> eded1;
+	//eded1 % 3 == 0 && cout << "3-e qaliqsiz bolunur" || cout << "3-e qaliqli bolunur";
+	//eded1 % 5 == 0 && cout << "5-e qaliqsiz bolunur" || cout << "5-e qaliqli bolunur";
+	//eded1 % 7 == 0 && cout << "7-e qaliqsiz bolunur" || cout << "7-e qaliqli bolunur";
+
+
+	////double eded_modul = 0;
+	////cout << "eded daxil edin: ";
+	////cin >> eded_modul;
+	//if (eded_modul < 0) cout << "ededin modulu: " << eded_modul * -1;
+	//else cout << eded_modul;
+
+
+	//double reqemli4 = 0;
+	//cout << "max 4 reqemli eded daxil edin: ";
+	//cin >> reqemli4 ;
+	//if (reqemli4>=1000) cout << "4 reqemli eded";
+	//else if (reqemli4 >= 100) cout << "3 reqemli eded";
+	//else if (reqemli4 >= 10) cout << "2 reqemli eded";
+	//else if (reqemli4 >= 1) cout << "1 reqemli eded";
+	//else cout << "sadece 4 reqemli eded daxil ede bilersiniz!!!";
+
+
+	//int polindrom = 0;
+	//cout << "5 reqemli eded daxil edin; ";
+	//cin >> polindrom;
+	//polindrom >=10000 && (float)(polindrom % 10) == (int)(polindrom / 1000) && (float)((polindrom - (float)(polindrom % 10)) - (int)(polindrom / 1000) % 10) == (int)((polindrom - (float)(polindrom % 10)) - (int)(polindrom / 1000) % 10) && cout << "eded polindrom deyil" || cout << "eded polindromdur";
+
+
+	//double konstant = 0;
+	//double sifre = 0;
+	//cout << "Konstant sifrenizi daxil edin: " << endl;
+	//cin >> konstant;
+	//cout << "Sifrenizi daxil edin: ";
+	//cin >> sifre;
+	//if (konstant == sifre) cout << " --Access succesfully completed-- ";
+	//else cout << " --Acces denied-- ";
+
+	//int insan = 0;
+	//cout << "Insan ilini daxil edin: ";
+	//cin >> insan;
+	//if (insan == 1) cout << 10.5;
+	//2 - insan <= 0 && cout << ((2 - insan) * -1) * 4 + 2 * 10.5 || cout << "Sadece musbet eded daxil edin!!!";
+}
+
